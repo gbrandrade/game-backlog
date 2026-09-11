6cc79631accff1b98087f00544a6edc4646251df
Gabriel Farias <gabriiel.farias@icloud.com>
2026-09-11 19:19:56 -0300

    fix: corrige indice do parametro em buscarPorId

--- src/main/java/io/github/gbrandrade/Main.java
+++ src/main/java/io/github/gbrandrade/Main.java
@@ -2,16 +2,12 @@ package io.github.gbrandrade;
 
 import io.github.gbrandrade.model.Plataforma;
 import io.github.gbrandrade.repository.PlataformaRepository;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         PlataformaRepository repository = new PlataformaRepository();
 
-        List<Plataforma> plataformas = repository.listarTodas();
-
-        for (Plataforma p : plataformas) {
-            System.out.println(p);
-        }
+        Plataforma plataforma = repository.buscarPorId(999);
+        System.out.println(plataforma);
     }
 }
